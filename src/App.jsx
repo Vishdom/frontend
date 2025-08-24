9649667024547f53e0224e24e3357d4bb7cae64a
vishdom <Vishpatilwork@gmail.com>
2025-08-24 09:14:58 +0530

    Initial commit: Vite + React Hello World

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World from Vish! 🎉</h1>
+      <p>This is my first React app running with Vite.</p>
+    </div>
+  );
+}
+
+export default App;
+
